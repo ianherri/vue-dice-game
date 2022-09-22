c80b676ca5b093c69a097d64e9741cba865d13a4
Ian Herrington <ianherri20@gmail.com>
2022-09-22 17:15:10 -0500

    play multiplayer with usernames

--- src/components/InitGame.vue
+++ src/components/InitGame.vue
@@ -1,24 +1,31 @@
 <template>
   <div>How many players are playing the game?</div>
   <form>
-    <select v-model="numPlayers" name="players" id="players">
-      <option value="1">one</option>
-      <option value="2">two</option>
-      <option value="3">three</option>
-      <option value="4">four</option>
-    </select>
-    <button @click.prevent="props.startGame(numPlayers)">start!</button>
+    <input placeholder="add a player" v-model="player" />
+    <button @click.prevent="addPlayer()">add player</button>
+    <br />
+    <br />
+    <button @click.prevent="props.startGame(players)">start!</button>
   </form>
 </template>
 
 <script setup>
+import { ref } from 'vue'
 import { defineProps } from 'vue'
 
 const props = defineProps({
   startGame: Function,
 })
 
-const numPlayers = 1
+const players = []
+
+const player = ref('')
+
+function addPlayer() {
+  players.push(player.value)
+  console.log(players)
+  player.value = ''
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
